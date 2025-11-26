bf2e8c194f59d9129ce0d8fcbfe0f9262b265ed1
CDMatherne <christopher.matherne@gmail.com>
2025-11-26 10:12:47 -0500

    Add files via upload

---
+++ ml_course_prediction/__init__.py
@@ -0,0 +1,8 @@
+"""
+ML Course Prediction Module
+Predicts future ship courses up to 48 hours ahead for law enforcement interdiction.
+"""
+
+__version__ = "1.0.0"
+__author__ = "ML Development Team"
+
